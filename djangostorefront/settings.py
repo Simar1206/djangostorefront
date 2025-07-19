fe448c7af3830703e5e0f4a9e49e845938effd03
Simar1206 <simars4064@gmail.com>
2025-07-20 03:17:01 +0530

    Fix typo in installed apps and update URL routing for playground app

--- djangostorefront/settings.py
+++ djangostorefront/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playgorund'
+    #new apps added
+    'playground'
 ]
 
 MIDDLEWARE = [
--- playground/urls.py
+++ playground/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('home/', views.say_hello)
+    path('hello/', views.say_hello)
 ]
